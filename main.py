e0ac68de7ccf5c73e42d40932f50b1a11d8c85d7
bp419 <58659650+bp419@users.noreply.github.com>
2021-06-12 22:23:49 +0100

    linearization

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from scipy.integrate import odeint
 
 st.title("A Visualisation of the Double Pendulum")
 st.sidebar.title("List of Parameters")
-time_taken = st.sidebar.slider("time taken for animation", 0, 100, 10)
 m1 = st.sidebar.number_input("input mass 1: ", min_value = 0, value = 1)
 m2 = st.sidebar.number_input("input mass 2: ", min_value = 0, value = 1)
 l1 = st.sidebar.number_input("input length 1: ", min_value = 0, value = 1)
@@ -21,33 +20,23 @@ initial_theta_2 = np.radians(st.sidebar.number_input("input inital displacement 
                                                      , max_value = 180 ))
 initial_vel_theta_1 = st.sidebar.number_input("input inital velocity of theta 1: "
                                               , value = 0)
-initial_vel_theta_2 = st.sidebar.number_input("input inital velocity of theta 2: "
+initial_vel_theta_2 = st.sidebar.number_input("input initial velocity of theta 2: "
                                               , value = 0)
-paths = st.sidebar.button("hide paths")
-other = st.sidebar.button("hide other trajectory")
+
 dt = 0.02
 eps = 0.05
+initial1 = np.array([initial_theta_1, initial_theta_2, initial_vel_theta_1,
+                     initial_vel_theta_2])
+initial2 = np.array([initial_theta_1, initial_theta_2 + eps, initial_vel_theta_1,
+                     initial_vel_theta_2])
+p = np.array([m1, m2, l1, l2, g])
+
 with st.beta_container():
     st.header("Introduction")
     st.markdown("This website is part of our group's M2R research project on the double pendulum at Imperial College London."
                 " Group members include Brendan Patalong, Frank Shang, Yian Zeng, James Hyrb and Mingke Peng with project supervisor"
-                " Dr Philip Ramsden. Below is an animation of the double pendulum system where the initial conditions and parameters"
-                " can be varied by the user using the inputs in the sidebar. Note that for the default values given we observe that"
-                " both pendulums in the animation quickly diverge despite bing fairly close to begin with. This is an example of"
-                " sensitive dependence on initial conditions or chaotic behaviour which is a characteristic property of the double pendulum"
-                " system for certain initial conditions. The aim of this project is to discuss further the behaviour"
-                " of the double pendulum system.")   
+                " Dr Philip Ramsden. The aim of this project is to discuss further the behaviour of the double pendulum  dynamical system.")   
 
-# use streamlit functions to take data inputs from user/
-time_array = np.arange(0, time_taken, dt)
-initial1 = np.array([initial_theta_1, initial_theta_2, initial_vel_theta_1,
-                    initial_vel_theta_2])
-initial2 = np.array([initial_theta_1, initial_theta_2 + eps, initial_vel_theta_1,
-                    initial_vel_theta_2])
-
-p = np.array([m1, m2, l1, l2, g])
-
-@st.cache
 def derivative(v, t):
     deriv = [0, 0, 0, 0]
     deriv[0] = v[2]
@@ -64,7 +53,7 @@ def derivative(v, t):
     deriv[3] = det ** (-1) * (A * f_2 - B * f_1)
     return deriv
 
-@st.cache
+
 def wrapped(theta, other):
     """Wrap angular displacement to obtain range of -pi to pi for infinite cylinder plot.
     
@@ -107,7 +96,7 @@ def wrapped(theta, other):
 
     return [theta_splitted, other_splitted]
 
-@st.cache
+
 def d_wrapped(theta_1, theta_2):
     """Wrap angular displacements theta_1, theta_2 to obtain range of -pi to pi for torus plot.
     
@@ -162,70 +151,80 @@ def d_wrapped(theta_1, theta_2):
         r += 1
         
     return [theta_1_splitted, theta_2_splitted]
-    
-
-soln_1 = odeint(derivative, initial1, time_array)
-soln_2 = odeint(derivative, initial2, time_array)
-
-#  convert to cartesian coordinates
-x_11 = p[2] * np.sin(soln_1[:, 0])
-y_11 = - p[2] * np.cos(soln_1[:, 0])
-x_12 = p[3] * np.sin(soln_1[:, 1]) + x_11
-y_12 = - p[3] * np.cos(soln_1[:, 1]) + y_11
-
-x_21 = p[2] * np.sin(soln_2[:, 0])
-y_21 = - p[2] * np.cos(soln_2[:, 0])
-x_22 = p[3] * np.sin(soln_2[:, 1]) + x_21
-y_22 = - p[3] * np.cos(soln_2[:, 1]) + y_21
-
-# first figure
-#fig1 = plt.figure(figsize=(5, 4))
-#sub = fig1.add_subplot(autoscale_on=False, 
-#                      xlim=(- (p[2] + p[3] + 1), (p[2] + p[3] + 1)),
-#                      ylim=(- (p[2] + p[3] + 1), (p[2] + p[3] + 1)))
-#sub.set_aspect('equal')
-#line1, = sub.plot([], [], "o-", lw=1.7, c = "blue")
-#line2, = sub.plot([], [], "o-", lw=1.7, c = "blue")
-#path1, = sub.plot([], [], ",-", lw=0.8, c = "black")
-#path2, = sub.plot([], [], ",-", lw=0.8, c = "blue")
-#line4, = sub.plot([], [], "o-", lw=1.7, c = "black")
-#line3, = sub.plot([], [], "o-", lw=1.7, c = "black")
-
-#def realisation(i):
-#    if not paths:
-#        path2.set_data(x_12[max(0, i-150):i], y_12[max(0, i-150):i])
-#        path1.set_data(x_22[max(0, i-150):i], y_22[max(0, i-150):i])
-#    if  not other:
-#        line3.set_data([0, x_21[i]], [0, y_21[i]])
-#        line4.set_data([x_21[i], x_22[i]], [y_21[i], y_22[i]])
-
-#    line1.set_data([0, x_11[i]], [0, y_11[i]])
-#    line2.set_data([x_11[i], x_12[i]], [y_11[i], y_12[i]])
-#
-#    return path1, path2, line3, line4, line1, line2
-
-# creation of the final animation
-#animator = animation.FuncAnimation(fig1, realisation, len(y_11),
-#                                     interval = dt*1000, blit=True)
-
-#with st.beta_container():
- #   components.html(animator.to_jshtml(), height=500)
 
 with st.beta_container():
-    st.header("Further Analysis")
+    st.header("Figures")
     st.markdown("For further analysis ")
-    dropbox = st.selectbox(label="Select a chart",options=("Linearization about (0,0,0,0)", 
-    "Phase Portraits", "Fast Fourier Transform", "Poincare Sections","Lyapunov Exponents"), 
-                index = 0)
+    dropbox = st.selectbox(label="Select a chart",options=("Double Pendulum Animation", "Linearization about (0,0,0,0)", 
+                                "Phase Portraits", "Fast Fourier Transform", "Poincare Sections","Lyapunov Exponents"), 
+                                index = 0)
+    
+with st.beta_container():
+    if dropbox == "Double Pendulum Animation":
+        time_taken = st.slider("time taken for animation", 0, 100, 10)
+        # use streamlit functions to take data inputs from user/
+        time_array = np.arange(0, time_taken, dt)
+        soln_1 = odeint(derivative, initial1, time_array)
+        soln_2 = odeint(derivative, initial2, time_array)
+        paths = st.button("hide paths")
+        other = st.button("hide other trajectory")
 
-#with st.beta_container():
-    if dropbox == "Linearization about (0,0,0,0)":
-        lamb1 = 1
-        lamb2 = 1
+        #  convert to cartesian coordinates
+        x_11 = p[2] * np.sin(soln_1[:, 0])
+        y_11 = - p[2] * np.cos(soln_1[:, 0])
+        x_12 = p[3] * np.sin(soln_1[:, 1]) + x_11
+        y_12 = - p[3] * np.cos(soln_1[:, 1]) + y_11
+
+        x_21 = p[2] * np.sin(soln_2[:, 0])
+        y_21 = - p[2] * np.cos(soln_2[:, 0])
+        x_22 = p[3] * np.sin(soln_2[:, 1]) + x_21
+        y_22 = - p[3] * np.cos(soln_2[:, 1]) + y_21
+
+        # first figure
+        fig1 = plt.figure(figsize=(5, 4))
+        sub = fig1.add_subplot(autoscale_on=False, 
+                            xlim=(- (p[2] + p[3] + 1), (p[2] + p[3] + 1)),
+                            ylim=(- (p[2] + p[3] + 1), (p[2] + p[3] + 1)))
+        sub.set_aspect('equal')
+        line1, = sub.plot([], [], "o-", lw=1.7, c = "blue")
+        line2, = sub.plot([], [], "o-", lw=1.7, c = "blue")
+        path1, = sub.plot([], [], ",-", lw=0.8, c = "black")
+        path2, = sub.plot([], [], ",-", lw=0.8, c = "blue")
+        line4, = sub.plot([], [], "o-", lw=1.7, c = "black")
+        line3, = sub.plot([], [], "o-", lw=1.7, c = "black")
+
+        def realisation(i):
+            if not paths:
+                path2.set_data(x_12[max(0, i-150):i], y_12[max(0, i-150):i])
+                path1.set_data(x_22[max(0, i-150):i], y_22[max(0, i-150):i])
+            if  not other:
+                line3.set_data([0, x_21[i]], [0, y_21[i]])
+                line4.set_data([x_21[i], x_22[i]], [y_21[i], y_22[i]])
+
+            line1.set_data([0, x_11[i]], [0, y_11[i]])
+            line2.set_data([x_11[i], x_12[i]], [y_11[i], y_12[i]])
+        
+            return path1, path2, line3, line4, line1, line2
+
+        # creation of the final animation
+        animator = animation.FuncAnimation(fig1, realisation, len(y_11),
+                                            interval = dt*1000, blit=True)
+
+        components.html(animator.to_jshtml(), height=500)
+
+
+    elif dropbox == "Linearization about (0,0,0,0)":
+        A = np.array([[-p[4] * (p[0] + p[1]) / (p[0] * p[2]), p[4] * p[1]/(p[0] * p[2])],
+                      [p[4] * (p[0] + p[1])/(p[0] * p[3]), p[4] * (p[1] + p[0])/(p[0] * p[3])]])
+        ev = np.linalg.eig(A)
+        lamb = np.sqrt(ev[0])
+        V = np.array([[ev[1][0]], [ev[1][1]]])
+        coefs_1 = np.linalg.solve(V, np.array([initial1[0],initial1[1]]))
+        t = np.linspace
 
     elif dropbox == "Phase Portraits":
-        time_taken = st.slider("Total Time", 10, 100, 10)
-        t = np.linspace(0, time_taken, 1001)
+        time_taken = st.slider("Total Time", 100, 1000, 100)
+        t = np.linspace(0, time_taken, 10001)
         sol = odeint(derivative, initial1, t)
 
         splitted_1 = wrapped(sol[:, 0], t)
